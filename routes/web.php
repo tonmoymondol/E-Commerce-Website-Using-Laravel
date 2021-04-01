621733458f9662b16b05c5b2387b4aedc76970ce
tonmoymondol <tonmoymondol04@gmail.com>
2021-04-02 02:16:42 +0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Session;
+use App\Http\Controllers\SslCommerzPaymentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('/login');
+});
+
+// Route::get('/', function () {
+//     return view('exampleEasycheckout');
+// });
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+Route::get('/exampleEasycheckout',[SslCommerzPaymentController::class,'exampleEasycheckout']);
+//SSLCOMMERZ END
+
+Route::view("/login","login");
+Route::view("/register","register");
+Route::post("login",[UserController::class,'login']);
+Route::post("register",[UserController::class,'register']);
+Route::get("/",[ProductController::class,'index']);
+Route::get("detail/{id}",[ProductController::class,'detail']);
+Route::get("search",[ProductController::class,'search']);
+Route::post("add_to_cart",[ProductController::class,'addToCart']);
+Route::get("cartlist",[ProductController::class,'cartList']); 
+Route::get("removecart/{id}",[ProductController::class,'removeCart']); 
+Route::get("ordernow",[ProductController::class,'orderNow']); 
+Route::post("orderplace",[ProductController::class,'orderPlace']); 
+Route::get("myorder",[ProductController::class,'myOrder']);  
